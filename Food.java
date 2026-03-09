2092d8de96bfc93338950d008aa50dd582bc663f
haiyinbruh <haiyin.qiu@jpschools.org>
2026-03-09 10:08:21 -0500

    Update Food.java

--- Food.java
+++ Food.java
@@ -1,3 +1,31 @@
-public class Food {
+
+public class Food{
+    private String foodName;
+    private int addEnergy;
+    private int addWeight;
+    private int addHappiness;
     
+public Food(String foodName, int addEnergy, int addWeight, int addHappiness){
+    this.foodName = foodName;
+    this.addEnergy = addEnergy;
+    this.addWeight = addWeight;
+    this.addHappiness = addHappiness;
+    
+}
+  
+  public String getName(){
+      return foodName;
+  }
+  
+  public int getAddEnergy(){
+      return addEnergy;
+  }
+  
+  public int getAddWeight(){
+      return addWeight;
+  }
+  
+  public int getAddHappiness(){
+      return addHappiness;
+  }
 }
