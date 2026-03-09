dda692db41cbbc34d5e0e8a8c60ef49b6b07a20e
haiyinbruh <haiyin.qiu@jpschools.org>
2026-03-09 10:08:34 -0500

    Update Game.java

--- Game.java
+++ Game.java
@@ -1,3 +1,32 @@
-public class Game {
+public class Game{
+    private String gameName;
+    private int addHappiness;
+    private int addWeight;
+    private int randomWin;
     
+    public Game(String gameName, int addHappiness, int addWeight){
+        this.gameName = gameName;
+        this.addHappiness = addHappiness;
+        this.addWeight = addWeight;
+    }
+    
+    public String getName(){
+        return gameName;
+    }
+    public int getAddHappiness(){
+        return addHappiness;
+    }
+    
+    public int getAddWeight(){
+        return addWeight;
+    }
+    public boolean hasWon(){
+        randomWin = (int)(Math.random()*10);
+        if (randomWin >=5){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
 }
